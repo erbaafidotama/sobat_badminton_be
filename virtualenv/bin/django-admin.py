4c64c01eaff600e7d2377e161590e9220bcdbeba
erb4f1dotama <erbaafidotama13@gmail.com>
2020-05-22 10:20:29 +0700

    DEL VIRENV

---
+++ virtualenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/erbaaafidotma/Documents/Project Python/Flask/sobat_badminton_be/virtualenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
